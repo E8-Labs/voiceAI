7e56f4456cb7bcc2aac53bc4672c78121b02cb6b
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-09-21 21:19:50 +0500

    static  data

--- components/Metatags/MetaTags.js
+++ components/Metatags/MetaTags.js
@@ -6,14 +6,11 @@ const MetaTags = ({ title, description, image }) => {
     return (
         <Head>
             <meta property="og:type" content="website" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
+            <meta property="og:title" content={"Andrew Tate"} />
+            <meta property="og:description" content={"Andrew tate's CreatorX"} />
+            <meta property="og:image" content={"https://static.independent.co.uk/2022/08/11/16/Andrew%20Tate.jpg?quality=75&width=1200&auto=webp"} />
             <meta property="og:url" content={typeof window !== 'undefined' ? window.location.href : ''} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
+            
             <title>{title}</title>
         </Head>
     );
