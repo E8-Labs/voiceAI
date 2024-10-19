cd9dfc01fa5519e346ec2ca7e40e4cb6e9a174df
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-10-19 20:54:36 +0500

    test static image

--- components/Metatags/MetaTags.js
+++ components/Metatags/MetaTags.js
@@ -27,19 +27,19 @@
 import React from 'react';
 import Head from 'next/head';
 
-const MetaTags = React.memo(({ title, description, image }) => {
-    console.log("Meta tags of Hamza tate are ", { title, description, image });
+const MetaTags = React.memo(({ title, description,  }) => {
+    console.log("Meta tags of Hamza tate are ", { title, description,  });
     return (
         <Head>
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image:secure_url" content={image} />
+            <meta property="og:image:secure_url" content="/myself.jpeg" />
             <meta property="og:url" content={typeof window !== 'undefined' ? window.location.href : ''} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
+            <meta name="twitter:image" content="myself.jpeg" />
             <title>{title}</title>
         </Head>
     );
